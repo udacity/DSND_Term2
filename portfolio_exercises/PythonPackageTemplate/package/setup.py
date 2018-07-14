ef496452eccd81329ca268dafe313f7b34956cd6
Andrew Paster <apaster@MacBook-Pro-4.local>
2018-07-13 17:12:10 -0700

    added OOP package template

---
+++ portfolio_exercises/PythonPackageTemplate/package/setup.py
@@ -0,0 +1,6 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
