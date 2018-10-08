306dc6da55cfde0c1fa6cd0ca0d1ba9df805800f
Andrew Paster <apaster@MacBook-Pro-4.local>
2018-10-08 13:04:19 -0700

    updated OOP notebooks

--- lessons/ObjectOrientedProgramming/JupyterNotebooks/1.OOP_syntax_shirt_practice/answer.py
+++ lessons/ObjectOrientedProgramming/JupyterNotebooks/1.OOP_syntax_shirt_practice/answer.py
@@ -7,12 +7,12 @@ from shirt import Shirt
 
 ### TODO:
 #    - insantiate a shirt object with the following characteristics:
-#        - color red, size S, style long sleeve, and price 25
+#        - color red, size S, style long-sleeve, and price 25
 #    - store the object in a variable called shirt_one
 #
 #
 ###
-shirt_one = Shirt('red', 'S', 'long sleeve', 25)
+shirt_one = Shirt('red', 'S', 'long-sleeve', 25)
 
 
 ### TODO:
@@ -35,22 +35,23 @@ print(shirt_one.discount(.12))
 #    - store the object in a variable called shirt_two
 #
 ###
-shirt_two = Shirt('orange', 'L', 'short sleeve', 10)
+shirt_two = Shirt('orange', 'L', 'short-sleeve', 10)
 
 
 ### TODO:
 #
 #    - calculate the total cost of shirt_one and shirt_two
+#    - store the results in a variable called total
 #    
 ###
-shirt_one.price + shirt_two.price
+total = shirt_one.price + shirt_two.price
 
 
 ### TODO:
 #
-#    - use the shirt discount methods to calculate the total cost if
+#    - use the shirt discount method to calculate the total cost if
 #       shirt_one has a discount of 14% and shirt_two has a discount
 #       of 6%
-#
+#    - store the results in a variable called total_discount
 ###
-shirt_one.discount(.14) + shirt_two.discount(.06)
+total_discount =  shirt_one.discount(.14) + shirt_two.discount(.06) 
--- lessons/ObjectOrientedProgramming/JupyterNotebooks/5.OOP_code_inheritance_clothing/answer.py
+++ lessons/ObjectOrientedProgramming/JupyterNotebooks/5.OOP_code_inheritance_clothing/answer.py
@@ -36,9 +36,9 @@ class Pants(Clothing):
         return self.price * (1 - discount / 2)
 
 class Blouse(Clothing):
-    def __init__(self, color, size, style, price, country_of_origen):
+    def __init__(self, color, size, style, price, country_of_origin):
         Clothing.__init__(self, color, size, style, price)
-        self.country_of_origen = country_of_origen
+        self.country_of_origin = country_of_origin
     
     def triple_price(self):
         return 3 * self.price
