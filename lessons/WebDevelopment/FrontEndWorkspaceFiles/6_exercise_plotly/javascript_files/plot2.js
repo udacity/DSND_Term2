65769ab7e095c2879a081d616104d4f6640c7fbb
OG <omargurashi1@gmail.com>
2021-03-19 14:54:52 +0400

    Created bar plot using plotly
    - Plot 2.js

--- lessons/WebDevelopment/FrontEndWorkspaceFiles/6_exercise_plotly/javascript_files/plot2.js
+++ lessons/WebDevelopment/FrontEndWorkspaceFiles/6_exercise_plotly/javascript_files/plot2.js
@@ -1,21 +1,26 @@
-var year = [2015];
-var arable_land_brazil = [33.8100342899258];
-var country_name_brazil = 'Brazil';
+var year = [2015]
+var arable_land_brazil = [33.8100342899258]
+var country_name_brazil = 'Brazil'
 
-var arable_land_germany = [47.9592041483809];
-var country_name_germany = 'Germany';
+var arable_land_germany = [47.9592041483809]
+var country_name_germany = 'Germany'
 
-var arable_land_china = [56.2229587724434];
-var country_name_china = 'China';
+var arable_land_china = [56.2229587724434]
+var country_name_china = 'China'
+
+var arable_land = []
 
 var trace1 = {
-/* TODO: Use the country name and arable land data to make a bar chart */
-};
+  x: [country_name_brazil, country_name_germany, country_name_china],
+  y: [arable_land_brazil[0], arable_land_germany[0], arable_land_china[0]],
+  type: 'bar',
+  name: 'omar',
+}
 
 var layout = {
- /* TODO: include a title for the chart */
-};
+  title: 'Arable land per country for the year ' + year,
+}
 
-var data = [trace1];
+var data = [trace1]
 
-Plotly.newPlot('plot2', data, layout);
+Plotly.newPlot('plot2', data, layout)
