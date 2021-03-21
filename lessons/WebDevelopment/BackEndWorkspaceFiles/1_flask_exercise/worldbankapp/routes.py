43ceb35cddbda6a6258354912a8f744324133781
OG <omargurashi1@gmail.com>
2021-03-21 17:15:58 +0400

    Added an app route and create HTML source code

--- lessons/WebDevelopment/BackEndWorkspaceFiles/1_flask_exercise/worldbankapp/routes.py
+++ lessons/WebDevelopment/BackEndWorkspaceFiles/1_flask_exercise/worldbankapp/routes.py
@@ -13,5 +13,8 @@ def project_one():
 
 # TODO: Add another route. You can use any names you want
 # Then go into the templates folder and add an html file that matches the file name you put in the render_template method. You can create a new file by going to the + sign at the top of the workspace and clicking on Create New File. Make sure to place the new html file in the templates folder.
+@app.route('/project-two')
+def project_two():
+    return render_template('project_two.html')
 
 # TODO: Start the web app per the instructions in the instructions.md file and make sure your new html file renders correctly.
