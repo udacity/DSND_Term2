ae509f65ab55fbb2934750de32c3fe141e9546ee
Clare Walker <clarew@quadraturecapital.com>
2021-03-26 20:27:16 +0000

    fixing adds

--- lessons/ObjectOrientedProgramming/IdeFiles/4a_binomial_package/distributions/__init__.py
+++ lessons/ObjectOrientedProgramming/IdeFiles/4a_binomial_package/distributions/__init__.py
@@ -1,3 +1,4 @@
 from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
 
 # TODO: import the Binomial class from the Binomialdistribution module
--- lessons/ObjectOrientedProgramming/IdeFiles/4a_binomial_package/setup.py
+++ lessons/ObjectOrientedProgramming/IdeFiles/4a_binomial_package/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='distributions',
-      version='0.1',
-      description='Gaussian distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
       packages=['distributions'],
       zip_safe=False)
